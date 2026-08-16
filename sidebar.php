f126fa33cac54ca1a17389ecbe607cdfa97b6ae2
2024-1-60-249-oss <2024-1-60-249@std.ewubd.edu>
2026-08-16 20:02:42 +0600

    Add files via upload

---
+++ sidebar.php
@@ -0,0 +1,20 @@
+<div class="sidebar">
+    <div>
+        <div class="brand">
+            <i class="fa-solid fa-graduation-cap"></i>
+            <h2>EAST WEST UNIVERSITY</h2>
+            <p>Student Management System</p>
+        </div>
+        <div class="nav-links">
+            <a href="dashboard.php" class="nav-item"><i class="fa-solid fa-chart-line"></i> Dashboard</a>
+            <a href="students.php" class="nav-item"><i class="fa-solid fa-user-graduate"></i> All Students</a>
+            <a href="add_student.php" class="nav-item"><i class="fa-solid fa-user-plus"></i> Add Student</a>
+            <a href="faculties.php" class="nav-item"><i class="fa-solid fa-chalkboard-user"></i> Faculties</a>
+            <a href="courses.php" class="nav-item"><i class="fa-solid fa-book"></i> Courses</a>
+            <a href="enrollments.php" class="nav-item"><i class="fa-solid fa-clipboard-list"></i> Enrollments</a>
+            <a href="results.php" class="nav-item"><i class="fa-solid fa-award"></i> Results & Grades</a>
+            <a href="users_manage.php" class="nav-item"><i class="fa-solid fa-users-gear"></i> User Accounts</a>
+        </div>
+    </div>
+    <a href="logout.php" class="nav-item logout"><i class="fa-solid fa-right-from-bracket"></i> Logout</a>
+</div>
